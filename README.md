e59b0d4de76c0579a5862a64921115656749e91f
shayal <shayalvaghasiya@gmail.com>
2024-01-27 18:03:37 +0530

    data ingestion added

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# End to end Text-Summarizer-Project
+# Text-Summarizer-Project
 
 ## Workflows
 
@@ -6,7 +6,7 @@
 2. Update params.yaml
 3. Update entity
 4. Update the configuration manager in src config
-5. update the conponents
+5. update the components
 6. update the pipeline
 7. update the main.py
 8. update the app.py
--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from TextSummarizer.pipeline.stage_01_data_ingetion import DataIngestionTrainingPipeline
 from TextSummarizer.logging import logger
 
-logger.info("Welcome to custome logging")
+SATEGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>>>stage {SATEGE_NAME} started <<<<<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>>> stage {SATEGE_NAME} completed <<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+ 
