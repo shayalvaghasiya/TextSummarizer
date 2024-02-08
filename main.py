b8524458a57688ea6018558098d991209a5b932e
shayal <shayalvaghasiya@gmail.com>
2024-02-08 14:21:07 +0530

    evaluation

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from TextSummarizer.pipeline.stage_01_data_ingetion import DataIngestionTraining
 from TextSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from TextSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from TextSummarizer.pipeline.stage_04_data_training import DataTrainingTrainingPipeline
+from TextSummarizer.pipeline.stage_05_data_evaluation import DataEvaluationTrainingPipeline
+
 from TextSummarizer.logging import logger
 
 SATEGE_NAME = "Data Ingestion stage"
@@ -46,6 +48,18 @@ SATEGE_NAME = "Data training stage"
 try:
     logger.info(f">>>>>>>stage {SATEGE_NAME} started <<<<<<<<<<")
     data_training = DataTrainingTrainingPipeline()
+    data_training.main()
+    logger.info(f">>>>>>>> stage {SATEGE_NAME} completed <<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+SATEGE_NAME = "Data evaluation stage"
+
+try:
+    logger.info(f">>>>>>>stage {SATEGE_NAME} started <<<<<<<<<<")
+    data_training = DataEvaluationTrainingPipeline()
     data_training.main()
     logger.info(f">>>>>>>> stage {SATEGE_NAME} completed <<<<<<<<")
 except Exception as e:
